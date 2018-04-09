dd5bac3b08f8d9f4f34e1750b12d7836acb53d5c
Justin Harris <jharris@amsmacx1ksh040.home>
2018-04-10 00:36:03 +0100

    Added comments

--- ha-failover-v2.ps1
+++ ha-failover-v2.ps1
@@ -1,45 +1,68 @@
 ﻿Write-Output -InputObject "PowerShell Timer trigger function executed at:$(Get-Date)"
+Write-Output -InputObject "PowerShell Timer trigger function executed at:$(Get-Date)"
 <#     
     Palo Alto Networks Traps PA-Azure Failover Example
-
     This script is an example for monitoring PA firewall status and performing
     fail-over and/or fail-back.
-
-    This script is used as part of an Azure Timer Trigger function.  As setup,
-    the following items must be configured:
+    This script is used as part of an Azure Function App called by a Timer Trigger event.  
+    
+    To setup, the following items must be configured:
     - Pre-create Azure ResourceGroups, virtual firewalls, Virtual Networks and Subnets
     - Create Azure Timer Function 
     - Set Function App Settings with credentials
-    SP_PASSWORD, SP_USERNAME, TENANTID must be added
+      SP_PASSWORD, SP_USERNAME, TENANTID, SUBSCRIPTIONID, AZURECLOUD must be added
+      AZURECLOUD = "AzureCloud" or "AzureUSGovernment"
+    - Set Firewall VM names and Resource Group in Function App Settings
+      FW1NAME, FW2NAME, FWMONITOR, FW1FQDN, FW1PORT, FW2FQDN, FW2PORT, FWRGNAME, FWTRIES, FWDELAY, FWUDRTAG must be added
+      FWMONITOR = "VMStatus" or "TCPPort" - If using "TCPPort", then also set FW1FQDN, FW2FQDN, FW1PORT and FW2PORT values
     - Set Timer Schedule where positions represent: Seconds - Minutes - Hours - Day - Month - DayofWeek
-    Example:  "*/30 * * * * *" to run on multiples of 30 seconds
-    Example:  "0 */5 * * * *" to run on multiples of 5 minutes on the 0-second mark
-    - Set script variables below
-    
-    Note:  $monitor = "VMStatus" or $monitor = "TCPPort" like examples here
-
-    $monitor = "VMStatus"
-    $rgName = "AzureRGName"
-    $vmFW1Name = "AzureFW1Name"
-    $vmFW2Name = "AzureFW2Name"
-    -- or -- 
-    $monitor = "TCPPort"
-    $tcpFW1Server = "www1.mydomain.com"
-    $tcpFW1Port = 80
-    $tcpFW2Server = "www2.mydomain.com"
-    $tcpFW2Port = 443
+      Example:  "*/30 * * * * *" to run on multiples of 30 seconds
+      Example:  "0 */5 * * * *" to run on multiples of 5 minutes on the 0-second mark
 #>
 
 #**************************************************************
 #          Set firewall monitoring variables here
 #**************************************************************
 
+$vmFW1Name = $env:FW1NAME              # Set the Name of the primary firewall
+$vmFW2Name = $env:FW2NAME              # Set the Name of the secondaryfirewall
+$Fw1RGName = $env:FWRGNAME             # Set the ResourceGroup that contains Fw1
+$Fw2RGName = $env:FWRGNAME             # Set the ResourceGroup that contains Fw2
+
+<#
+    Set the parameter $monitor to  "VMStatus" if the current state 
+    of the firewall is monitored.  The firewall will be marked as 
+    down if we do not receive a "running response to the api call"
+    Set the parameter $monitor to "TCPPort"  if the forwarding state
+    of the firewall is to be tested by connecting through the firewall 
+    to remote sites
+#>
+
+$monitor = $env:FWMONITOR              # "VMStatus" or "TCPPort" are valid values
+
+#**************************************************************
+#    The parameters below are required if using "TCPPort" mode
+#**************************************************************
+
+$tcpFW1Server = $env:FW1FQDN   # Hostname of the site to be monitored if using "TCPPort"
+$tcpFW1Port = $env:FW1PORT
+$tcpFW2Server = $env:FW2FQDN
+$tcpFW2Port = $env:FW2PORT
+
+#**************************************************************
+#    Set the failover and failback behaviour for the firewalls
+#**************************************************************
+
+$FailOver = $True          # Trigger to enable fail-over to FW2 if FW1 drops when active
+$FailBack = $True          # Trigger to enable fail-back to FW1 if FW2 drops when active
+
+# FW is deemed down if ALL intTries fail with intSeconds between tries
+
+$intTries = $env:FWTRIES          # Number of Firewall tests to try 
+$intSleep = $env:FWDELAY          # Delay in seconds between tries
 
 
-$vmFW1Name = 'VM-FW1'              # Set the Name of the primary firewall
-$vmFW2Name = 'VM-FW2'              # Set the Name of the secondaryfirewall
-$Fw1RGName = 'fwhademo'       # Set the ResourceGroup that contains Fw1
-$Fw2RGName = 'fwhademo'       # Set the ResourceGroup that contains Fw2
+
 
 
 <# Set the subscription that contains the ResourceGroups for the firewall
@@ -54,16 +77,7 @@ $defaultsubscriptionID = ''
 
 $defaultsubscriptionID = '' 
 
-<#
-    Set the parameter $monitor to  "VMStatus" if the current state 
-    of the firewall is monitored.  The firewall will be marked as 
-    down if we do not receive a "running response to the api call"
 
-    Set the parameter $monitor to "TCPPort"  if the forwarding state
-    of the firewall is to be tested by connecting through the firewall 
-    to remote sites
-#>
-$monitor = 'VMStatus'
 
 <#
     Set the failover method.  Secondary-int will move the secondary ip 
@@ -76,9 +90,9 @@ $monitor = 'VMStatus'
    
     $Script:IpconfigName is the IPconfiguration Name for the secondary IP address
 #>
-$failoverMode ='secondary-int'
+$failoverMode =  $env:FAILMODE # Use secondary-int if moving a secondary IP address"
 #$failoverMode = 'route-table'
-$Script:IpconfigName = ''
+$Script:IpconfigName = $env:IPCONFIGNAME
 
 <#
     TCPPort monitoring is most applicable when the azure function is deployed within
@@ -150,8 +164,8 @@ if ($failoverMode -eq 'route-table')
 
   foreach ($subscriptionID in $Script:listofsubscriptionIDs){
   Set-AzureRmContext -SubscriptionId $subscriptionID
-  $rtable = @()
-  $res = Find-AzureRmResource -ResourceType microsoft.network/routetables
+  $tagValue = $env:FWUDRTAG
+  $res = Find-AzureRmResource -TagName pan_ha_udr -TagValue $tagValue
 
   foreach ($rtable in $res)
     {
@@ -203,7 +217,8 @@ if ($failoverMode -eq 'route-table')
 {
   foreach ($subscriptionID in $Script:listofsubscriptionIDs){
   Set-AzureRmContext -SubscriptionId $subscriptionID
-  $res = Find-AzureRmResource -ResourceType microsoft.network/routetables
+  $tagValue = $env:FWUDRTAG
+  $res = Find-AzureRmResource -TagName pan_ha_udr -TagValue $tagValue
 
   foreach ($rtable in $res)
   {
@@ -353,10 +368,10 @@ Function getfwinterfaces {
 }
  
 Function getallsubscriptions {
-  $subs = Get-AzureRmContext
-  foreach ($sub in $subs){
-    $Script:listofsubscriptionIDs += $sub.Subscription.Id
-  }
+  Write-Output -InputObject "Enumerating all subscriptins ..."
+  $Script:listofsubscriptionIDs = (Get-AzureRmSubscription).SubscriptionId
+  Write-Output -InputObject $Script:listofsubscriptionIDs
+
 }
 
 
@@ -365,23 +380,23 @@ Function getallsubscriptions {
     #**************************************************************
 
     # Set Service Principal Credentials and establish your Azure RM Context
-    # SP_PASSWORD, SP_USERNAME, TENANTID, SUBSCRIPTIONID are app settings
+    # SP_PASSWORD, SP_USERNAME, TENANTID, SUBSCRIPTIONID and AZURECLOUD are app settings
 #>
 
 
-<#$password = ConvertTo-SecureString $env:SP_PASSWORD -AsPlainText -Force
+$password = ConvertTo-SecureString $env:SP_PASSWORD -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential ($env:SP_USERNAME, $password)
-Add-AzureRmAccount  -Tenant $env:TENANTID -Credential $credential -SubscriptionId $env:SUBSCRIPTIONID
+$azureEnv = Get-AzureRmEnvironment -Name $env:AZURECLOUD
+Add-AzureRmAccount -ServicePrincipal -Tenant $env:TENANTID -Credential $credential -SubscriptionId $env:SUBSCRIPTIONID -Environment $azureEnv
 
 $context = Get-AzureRmContext
 Set-AzureRmContext -Context $context
+
+<#
+Use Login-AzureRmAccount if testing from a local machine
+Use $env:SP_PASSWORD and $env:SP_USERNAME if used in an Azure Function
+Login-AzureRmAccount
 #>
-
-#Use Login-AzureRmAccount if testing from a local machine
-#Use $env:SP_PASSWORD and $env:SP_USERNAME if used in an Azure Function
-#Login-AzureRmAccount
-$context = Get-AzureRmContext
-Set-AzureRmContext -Context $context
 
 $Script:PrimaryInts = @()
 $Script:SecondaryInts = @()
